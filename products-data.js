5c4c67eca46ed3edb99652666d77d183d9c40969
phoenixborneoath <scorched.oath@gmail.com>
2025-12-20 20:44:50 +0700

    Create products-data.js

---
+++ products-data.js
@@ -0,0 +1,61 @@
+// ====== PHOENIXBORNE OATH - PRODUCT DATA ======
+
+const PRODUCTS = [
+  {
+    id: 1,
+    name: "PHOENIXBORNE TEE",
+    slug: "phoenix-borne-tee",
+    category: "limited",
+    price: 289000,
+    originalPrice: null,
+    badge: "First article",
+    isNew: false,
+    discount: "New Arrival",
+    stockStatus: "Ready Stock",
+
+    sizes: {
+      S: true,
+      M: true,
+      L: true,
+      XL: true,
+      XXL: false
+    },
+
+    images: [
+      "images/pborne.jpg",
+      "images/petir.jpg",
+      "images/size.jpg"
+    ],
+
+    cover: "images/pborne.jpg"
+  },
+
+  {
+    id: 2,
+    name: "COMING SOON TEE",
+    slug: "coming-soon-tee",
+    category: "soldout",
+    price: 0,
+    originalPrice: null,
+    badge: "Sold Out",
+    isNew: false,
+    discount: "",
+    stockStatus: "Sold Out",
+
+    sizes: {
+      S: false,
+      M: false,
+      L: false,
+      XL: false,
+      XXL: false
+    },
+
+    images: [
+      "images/cooming.jpg",
+      "images/petir.jpg",
+      "images/size.jpg"
+    ],
+
+    cover: "images/cooming.jpg"
+  }
+];
