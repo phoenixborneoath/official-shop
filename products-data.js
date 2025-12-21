36963a3e4088b6593c48dacc3e40f2d184c3d692
phoenixborneoath <scorched.oath@gmail.com>
2025-12-21 20:25:32 +0700

    Update products-data.js

--- products-data.js
+++ products-data.js
@@ -3,7 +3,7 @@
 const PRODUCTS = [
   {
     id: 1,
-    name: "PHOENIXBORNE TEE",
+    name: "COOMING SOON",
     slug: "phoenix-borne-tee",
     category: "season1",
     price: 289000,
@@ -18,36 +18,7 @@ const PRODUCTS = [
       M: true,
       L: true,
       XL: true,
-      XXL: false
-    },
-
-    images: [
-      "images/pborne.jpg",
-      "images/petir.jpg",
-      "images/size.jpg"
-    ],
-
-    cover: "images/pborne.jpg"
-  },
-
-  {
-    id: 2,
-    name: "COMING SOON TEE",
-    slug: "coming-soon-tee",
-    category: "soldout",
-    price: 0,
-    originalPrice: null,
-    badge: "Sold Out",
-    isNew: false,
-    discount: "",
-    stockStatus: "Sold Out",
-
-    sizes: {
-      S: false,
-      M: false,
-      L: false,
-      XL: false,
-      XXL: false
+      XXL: true,
     },
 
     images: [
@@ -57,34 +28,5 @@ const PRODUCTS = [
     ],
 
     cover: "images/cooming.jpg"
-    },
-
-  {
-    id: 3,
-    name: "PHOENIXBORNE TEE",
-    slug: "phoenix-borne-tee2",
-    category: "season1",
-    price: 289000,
-    originalPrice: null,
-    badge: "new article",
-    isNew: false,
-    discount: "New Arrival",
-    stockStatus: "Ready Stock",
-
-    sizes: {
-      S: true,
-      M: true,
-      L: true,
-      XL: true,
-      XXL: false
-    },
-
-    images: [
-      "images/petir.jpg",
-      "images/petir.jpg",
-      "images/size.jpg"
-    ],
-
-    cover: "images/petir.jpg"
   }
 ];
