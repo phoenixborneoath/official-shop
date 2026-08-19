50f7d7eadde367c28f40297f0d19957f9de2c690
phoenixborneoath <scorched.oath@gmail.com>
2026-08-19 19:21:46 +0700

    Update products-data.js

--- products-data.js
+++ products-data.js
@@ -28,5 +28,63 @@ const PRODUCTS = [
   "images/phoenixborne-oath-broken-gate-size-chart1.jpg"
 ],
 cover: "images/phoenixborne-oath-broken-gate-cover.jpg"
+ },
+
+  {
+    id: 2,
+    name: "COMING SOON TEE",
+    slug: "coming-soon-tee",
+    category: "soldout",
+    price: 0,
+    originalPrice: null,
+    badge: "Sold Out",
+    isNew: false,
+    discount: "",
+    stockStatus: "Sold Out",
+
+    sizes: {
+      S: false,
+      M: false,
+      L: false,
+      XL: false,
+      XXL: false
+    },
+
+    images: [
+      "images/cooming.jpg",
+      "images/petir.jpg",
+      "images/size.jpg"
+    ],
+
+    cover: "images/cooming.jpg"
+    },
+
+  {
+    id: 3,
+    name: "PHOENIXBORNE TEE",
+    slug: "phoenix-borne-tee2",
+    category: "season1",
+    price: 289000,
+    originalPrice: null,
+    badge: "new article",
+    isNew: false,
+    discount: "New Arrival",
+    stockStatus: "Ready Stock",
+
+    sizes: {
+      S: true,
+      M: true,
+      L: true,
+      XL: true,
+      XXL: false
+    },
+
+    images: [
+      "images/petir.jpg",
+      "images/petir.jpg",
+      "images/size.jpg"
+    ],
+
+    cover: "images/petir.jpg"
   }
 ];
